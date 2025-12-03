663ade5ac75ff9b622b742da82e25806eebd10dd
joliebao <baojolie@gmail.com>
2025-12-03 01:07:31 -0500

    Part 2 fixed; matches Mr. Holmer's solution because my solution did not iterate through every turn one by one.

--- src/Day1.java
+++ src/Day1.java
@@ -12,39 +12,67 @@ public class Day1 {
     }
 
     // part 1: find amount of times it is rotated at 0
-    public void rotate(String rotation){
+    public int rotate1(String rotation){
         String direction = rotation.substring(0,1);
         int turnValue = Integer.parseInt(rotation.substring(1));
-
+        while (turnValue > 99){
+            turnValue -= 100;
+        }
+        System.out.println(turnValue);
         if (direction.equals("L")){
             dial -= turnValue;
-            while (dial < 0){
-                dial += 100;
-                passesZero++;
+            if (dial < 0){
+                dial = 100 + dial; // because it is negative add dial amount
+            } else if (dial > 99){
+                dial = dial - 100; // reset back to 0 + dial overflow
             }
         } else {
             dial += turnValue;
-            while (dial > 99){
-                dial -= 100;
-                passesZero++;
+            if (dial > 99){
+                dial = dial - 100;
+            } else if (dial < 0){
+                dial = 100 + dial;
             }
         }
+        System.out.println(dial);
+        return dial;
     }
 
     public int atZero(){
         for (String r : input){
-            rotate(r);
+            rotate1(r);
             if (dial == 0) zeroes++;
         }
         return zeroes;
     }
 
     // Part 2
+    public void rotate2(String rotation){
+        String direction = rotation.substring(0,1);
+        int turnValue = Integer.parseInt(rotation.substring(1));
+
+        // iterates through every value
+        if (direction.equals("L")){
+            while (turnValue > 0){
+                dial--;
+                if (dial == 0) passesZero++;
+                if (dial == -1) dial = 99;
+                turnValue--;
+            }
+        } else {
+            while (turnValue > 0){
+                dial++;
+                dial %= 100;
+                if (dial == 0) passesZero++;
+                turnValue--;
+            }
+        }
+    }
+
     public int getPassesZero(){
+        dial = 50;
         for (String r : input){
-            rotate(r);
-            System.out.println(passesZero);
-            if (dial == 0) passesZero++;
+            rotate2(r);
         }
         return passesZero;
     }
