e9e0323085092f0b3598aaed17d89e480ec0dcec
JolieBao <@>
2025-12-09 10:20:07 -0500

    Working on part 1 (will test at home)

--- src/Day5.java
+++ src/Day5.java
@@ -3,8 +3,11 @@ import java.util.ArrayList;
 public class Day5 {
     private ArrayList<String> input = new ArrayList<String>();
     private ArrayList<String> ranges = new ArrayList<String>();
-    private int lower;
-    private int higher;
+    private long lower;
+    private long higher;
+    private boolean fresh;
+    private int counter;
+    private int count;
 
     public Day5(ArrayList<String> list){
         input = list;
@@ -18,7 +21,29 @@ public class Day5 {
     }
 
     public boolean getFresh(){
-        return false; // placeholder
+        for (int i = 0; i < ranges.size(); i++){
+            String range = ranges.get(i);
+            lower = Long.parseLong(range.substring(0, range.indexOf("-")));
+            higher = Long.parseLong(range.substring(range.indexOf("-") + 1));
+            long num = Long.parseLong(input.get(counter));
+            if (num > lower && num < higher){
+                   input.remove(counter);
+                   counter--;
+            }
+            counter++;
+        }
+        return fresh; // placeholder
+    }
+
+    public int getSum(){
+        counter = 0;
+        while (!fresh || counter < input.size()){
+            if (getFresh()){
+                count++;
+            }
+            counter ++;
+        }
+        return count;
     }
 
     // first separate lines with dashes and just numbers
