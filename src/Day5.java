28eed4756bb2ba80e02a88e359d9759608ac69a2
joliebao <baojolie@gmail.com>
2025-12-11 02:03:07 -0500

    Tried the list in list and it's not working. Will see again tomorrow.

--- src/Day5.java
+++ src/Day5.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Day5 {
@@ -9,10 +9,10 @@ public class Day5 {
     private long higher;
     private int count;
 
-    public Day5(ArrayList<String> list){
+    public Day5(ArrayList<String> list) {
         input = list;
-        for (int i = 0; i < input.size(); i++){
-            if (input.get(i).contains("-")){
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i).contains("-")) {
                 ranges.add(input.get(i));
                 input.remove(i);
                 i--;
@@ -20,22 +20,22 @@ public class Day5 {
         }
     }
 
-    // part 1
-    public boolean isFresh(long num){
-        for (String range: ranges){
+    public boolean isFresh(long num) {
+        for (int i = 0; i < ranges.size(); i++) {
+            String range = ranges.get(i);
             lower = Long.parseLong(range.substring(0, range.indexOf("-")));
             higher = Long.parseLong(range.substring(range.indexOf("-") + 1));
-            if (num <= higher && num >= lower){
+            if (num <= higher && num >= lower) {
                 return true;
             }
         }
         return false;
     }
 
-    public int getSum(){
-        for (String id: input){
+    public int getSum() {
+        for (String id : input) {
             long num = Long.parseLong(id);
-            if (isFresh(num)){
+            if (isFresh(num)) {
                 count++;
             }
         }
@@ -43,43 +43,56 @@ public class Day5 {
     }
 
     // part 2
-    private void sortRange(){
-        for (int i = 1; i < ranges.size(); i++){
+    private void sortRange() {
+        for (int i = 1; i < ranges.size(); i++) {
             // sorts by length
             String r2 = ranges.get(i);
-            String r1 = ranges.get(i-1);
+            String r1 = ranges.get(i - 1);
             long l2 = Long.parseLong(r2.substring(0, r2.indexOf("-")));
             long l1 = Long.parseLong(r1.substring(0, r1.indexOf("-")));
-            while (l2 < l1 && i >= 1){
-                ranges.add(i-1, ranges.get(i));
-                ranges.remove(i+1);
+            while (l2 < l1 && i >= 1) {
+                ranges.add(i - 1, ranges.get(i));
+                ranges.remove(i + 1);
                 i--;
             }
         }
     }
 
+    private ArrayList<Long[]> makeList() {
+        ArrayList<Long[]> l = new ArrayList<Long[]>();
+        for (int i = 1; i < ranges.size(); i++) {
+            String r = ranges.get(i);
+            long lower = Long.parseLong(r.substring(0, r.indexOf("-")));
+            long upper = Long.parseLong(r.substring(r.indexOf("-")));
+            System.out.println(lower);
+            System.out.println(upper);
+            Long[] a = new Long[]{lower, upper};
+            l.add(a);
+        }
+        return l;
+    }
 
-    public long getMaxFresh(){
-        sortRange();
-        System.out.println(ranges);
+    public long getMaxFresh() {
+        ArrayList<Long[]> list = makeList();
+        for (Long[] l : list){
+            System.out.println(Arrays.toString(l));
+        }
         long total = 0;
         long lastUpper;
-        for (int i = 0; i < ranges.size(); i++){
-            if (i == 0){
+        for (int i = 0; i < ranges.size(); i++) {
+            if (i == 0) {
                 lastUpper = 0;
             } else {
-                String lastRange = ranges.get(i-1);
+                String lastRange = ranges.get(i - 1);
                 lastUpper = Long.parseLong(lastRange.substring(lastRange.indexOf("-") + 1));
             }
             String range = ranges.get(i);
             lower = Long.parseLong(range.substring(0, range.indexOf("-")));
-            if (lower < lastUpper){
+            if (lower < lastUpper) {
                 lower = lastUpper + 1;
             }
             higher = Long.parseLong(range.substring(range.indexOf("-") + 1));
-            System.out.println(higher - lower);
             total += (higher - lower) + 1;
-            System.out.println(total);
         }
         return total;
     }
