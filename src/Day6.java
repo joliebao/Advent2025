2f87e91b95b2b960eee9f9831b3ee28a425c51de
JolieBao <@>
2025-12-12 10:19:25 -0500

    Day 6 part 1 almost done!

--- src/Day6.java
+++ src/Day6.java
@@ -1,28 +1,58 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Day6 {
     private ArrayList<String> input = new ArrayList<String>();
-
+    private ArrayList<String> operations;
+    private ArrayList<Integer> results = new ArrayList<Integer>();
 
     public Day6(ArrayList<String> list) {
         input = list;
     }
 
     // part 1
-
     // find way to separate by any num of spaces
-    // separate operations
     // for each operations, run through lind
     private void lineMath(String line){
-        line = line.replaceAll("\\s", "-");
-        String[] StringNum = line.split("-");
-        System.out.println(Arrays.toString(StringNum));
+        ArrayList<String> numbers = new ArrayList<>(Arrays.asList(line.split(" ")));
+        for (int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i).isBlank()) {
+                numbers.remove(i);
+                i--;
+            } else {
+                String num = numbers.get(i);
+                numbers.set(i, num.trim());
+            }
+        }
+
+        for (int i = 0; i < numbers.size(); i++){
+            int num = Integer.parseInt(numbers.get(i));
+            if (Objects.equals(operations.get(i), "*")){
+                num *= results.get(i);
+                results.set(i, num);
+            }
+        }
     }
 
     public long doMath(){
-        for (String problem : input){
-            lineMath(problem);
+        String ops = input.get(4);
+        operations = new ArrayList<>(Arrays.asList(ops.split(" ")));
+        for (int i = 0; i < operations.size(); i++){
+            if (operations.get(i).contains("*")){
+                operations.set(i, "*");
+                results.add(1);
+            } else if (operations.get(i).contains("+")) {
+                operations.set(i, "+");
+                results.add(0);
+            } else {
+                operations.remove(i);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < input.size() - 1; i++){
+            lineMath(input.get(i));
         }
         return 0; // PLACEHOLDER
     }
