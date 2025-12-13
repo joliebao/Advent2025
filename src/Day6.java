c18da25a12743e5d17f2eec667712c6c6d29e6d7
joliebao <baojolie@gmail.com>
2025-12-13 01:56:02 -0500

    Day 6 part 1 done!

--- src/Day6.java
+++ src/Day6.java
@@ -5,15 +5,14 @@ import java.util.Objects;
 public class Day6 {
     private ArrayList<String> input = new ArrayList<String>();
     private ArrayList<String> operations;
-    private ArrayList<Integer> results = new ArrayList<Integer>();
+    private ArrayList<Long> results = new ArrayList<Long>();
+    private Long ans=0L;
 
     public Day6(ArrayList<String> list) {
         input = list;
     }
 
     // part 1
-    // find way to separate by any num of spaces
-    // for each operations, run through lind
     private void lineMath(String line){
         ArrayList<String> numbers = new ArrayList<>(Arrays.asList(line.split(" ")));
         for (int i = 0; i < numbers.size(); i++){
@@ -27,24 +26,27 @@ public class Day6 {
         }
 
         for (int i = 0; i < numbers.size(); i++){
-            int num = Integer.parseInt(numbers.get(i));
+            Long num = Long.parseLong(numbers.get(i));
             if (Objects.equals(operations.get(i), "*")){
                 num *= results.get(i);
+                results.set(i, num);
+            } else if (Objects.equals(operations.get(i), "+")){
+                num += results.get(i);
                 results.set(i, num);
             }
         }
     }
 
     public long doMath(){
-        String ops = input.get(4);
+        String ops = input.get(input.size() - 1);
         operations = new ArrayList<>(Arrays.asList(ops.split(" ")));
         for (int i = 0; i < operations.size(); i++){
             if (operations.get(i).contains("*")){
                 operations.set(i, "*");
-                results.add(1);
+                results.add(1L);
             } else if (operations.get(i).contains("+")) {
                 operations.set(i, "+");
-                results.add(0);
+                results.add(0L);
             } else {
                 operations.remove(i);
                 i--;
@@ -54,6 +56,11 @@ public class Day6 {
         for (int i = 0; i < input.size() - 1; i++){
             lineMath(input.get(i));
         }
-        return 0; // PLACEHOLDER
+
+        for (long num: results){
+            ans += num;
+        }
+
+        return ans;
     }
 }
